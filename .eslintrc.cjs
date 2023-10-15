bc3239e160af829c203da89c785889dfb2318719
rahmansodev <rahmansodev@gmail.com>
2023-10-15 11:31:16 +0800

    fix: install eslint-config-airbnb-base and eslint-plugin-import to make the linting works

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'airbnb-base',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'no-underscore-dangle': 'off',
+		'no-unused-vars': 'warn',
+		'import/no-named-as-default': 'off',
+		'import/no-named-as-default-member': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-cycle': 'off',
+		'comma-dangle': ['warn', 'always-multiline'],
+		// no "any" in PropTypes
+		'no-empty-pattern': 'warn',
+		// disable warning linebreak lf crlf
+		'linebreak-style': 0,
+		'no-tabs': 'off',
+	},
+};
