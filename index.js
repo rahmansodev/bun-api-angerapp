fdcbe5e126b74b4e8c72ee4b6f0e9f90a387d392
rahmansodev <rahmansodev@gmail.com>
2023-10-16 08:46:45 +0800

    fix: test fail commit

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');      
+	res.send('Hello World!');  
 });
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}...`);
 });
